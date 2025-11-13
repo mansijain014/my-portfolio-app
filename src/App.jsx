a9960f051c9ba292dd5b55b3b41735dd4016dc56
Jain Mansi <T0283180@gemalto.com>
2025-11-14 00:55:54 +0530

    Uncommented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import ShowreelSection from "./components/ShowReelSection";
-// import BrandPartners from "./components/BrandPartners";
+import BrandPartners from "./components/BrandPartners";
 import ServicesSection from "./components/ServiceSection";
-// import TestimonialsSection from "./components/Testimonials";
+import TestimonialsSection from "./components/Testimonials";
 import Footer from "./components/Footer";
 import Albums from "./components/Albums";
 
@@ -13,10 +13,10 @@ function App() {
       <Navbar />
       <Hero />
       <ShowreelSection />
-      {/* <BrandPartners /> */}
+      <BrandPartners />
       <Albums />
       <ServicesSection />
-      {/* <TestimonialsSection /> */}
+      <TestimonialsSection />
       <Footer />
     </div>
   );
