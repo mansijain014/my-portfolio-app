1ae770e9514e2adfdb6976b5669f9ba49eeb9c49
Jain Mansi <T0283180@gemalto.com>
2025-11-14 00:53:42 +0530

    Fixed filename in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import ShowreelSection from "./components/ShowReelSection";
 // import BrandPartners from "./components/BrandPartners";
