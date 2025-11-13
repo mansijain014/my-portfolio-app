63debec4dbb4cc2a726c51d29f739ff91a0327d2
Jain Mansi <T0283180@gemalto.com>
2025-11-14 00:49:36 +0530

    Commented files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ShowreelSection from "./components/ShowReelSection";
-import BrandPartners from "./components/BrandPartners";
+// import BrandPartners from "./components/BrandPartners";
 import ServicesSection from "./components/ServiceSection";
-import TestimonialsSection from "./components/Testimonials";
+// import TestimonialsSection from "./components/Testimonials";
 import Footer from "./components/Footer";
 import Albums from "./components/Albums";
 
