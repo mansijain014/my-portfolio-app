fc10a00663f2f3887c13e64218bc9f9e39b50ba4
Jain Mansi <T0283180@gemalto.com>
2025-11-18 17:21:16 +0530

    Added ScrollToTop utility

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,6 +8,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
