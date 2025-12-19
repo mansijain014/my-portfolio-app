07ad15960054a3a05cd9d4d5af2682ce44f3414c
Jain Mansi <T0283180@gemalto.com>
2025-12-19 16:40:51 +0530

    Fixed some CSS

--- src/pages/Digitals.jsx
+++ src/pages/Digitals.jsx
@@ -192,8 +192,7 @@ export default function CreativeServicesSplit() {
                       {sec.items.map((item, i) => (
                         <div
                           key={i}
-                          className="px-4 py-2 rounded-xl bg-white/10 border border-white/10 
-                                     hover:bg-purple-400/20 hover:border-purple-400/40 
+                          className="px-4 py-2 rounded-xl bg-white/10 border border-white/10
                                      transition-all text-sm md:text-base"
                         >
                           {item}
